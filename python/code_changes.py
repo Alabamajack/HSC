52bf6680f1124f2d2603cf79d6f3d257d41861ee
Philipp-e <ph.eidenschink@gmail.com>
2016-03-29 13:53:11 +0200

    Merged changes on python Skript and deleted folder "aufgabe3"

--- python/code_changes.py
+++ python/code_changes.py
@@ -55,11 +55,13 @@ def variable_renaming(string_to_rename_variables):
     """
     new_file_string = ""
     variables = {}
+    target_variable = []
+    known_variables = 0
     for line in string_to_rename_variables:
         variable = ""
         # do some stuff with each line
-        line.lstrip('\r\n ')
-        line.rstrip('\r\n ')
+        line = line.lstrip('\r\n ')
+        line = line.rstrip('\r\n ') #Remove '\r' and '\n' if any
         left_side_variable = "" #for knowing what variable is on the left side of the operation
         for sign in line:
             if not is_operator(sign):
@@ -69,27 +71,37 @@ def variable_renaming(string_to_rename_variables):
                 variable = variable.lstrip()
                 variable = variable.rstrip()
                 if variable != "":
-                    if is_equal_sign(sign):
+                    if is_equal_sign(sign): #variable left of the equal sign
+                        known_variables = known_variables +1
                         # do some stuff with the one variable on the left side of the operation
-                        var_counter = (0 if variable not in variables else variables[variable])
-                        variables[variable] = var_counter + 1 #i have this variable at least one time at the left side. so this is not the same variable, if i see it on the right side
+                        var_counter = (0 if variable not in variables else variables[variable][1])
+                        
+                        if var_counter == 0: #This variable is unknown, but on the left side
+                            variables[variable] = [known_variables, 1]
+                            known_variables = known_variables +1 
+                        
                         left_side_variable = variable
+                        
                         for i in range(var_counter + 1): #adding the $ d
                             variable += '$'
-                    else:
-                        # the right side of the assignment
-                        if variable in variables:
-                            for i in range(variables[variable] - (1 if left_side_variable == variable else 0)):
-                                # do this -1 times if the variable was the left side operator, because the counter of the variable was incremented
+                            variables[variable.rstrip('$')][1] = i + 1 #Note number of $ on original variable
+                        variables[variable] = [known_variables, 0]
+                        target_variable.append('#' + str(variables[variable][0]))
+                    else: # the right side of the assignment
+                        if variable in variables: #variable already known
+                            for i in range(variables[variable][1] - (1 if left_side_variable == variable else 0)):
                                 variable += '$'
-                        else:
-                            variables[variable] = 0
+                        else: #variable not known
+                            known_variables = known_variables +1
+                            variables[variable] = [known_variables, 0] #Use new variable
+                new_file_string += ('#' + str(variables[variable][0]) if variable != '' else '') + sign if is_operator(sign) else ""
                 # zeroing variable for next one
-                new_file_string += variable + sign if is_operator(sign) else ""
                 variable = ""
+        new_file_string = new_file_string.rstrip(';') #Remove ';'
         new_file_string += "\n"
         left_side_variable = ""
-    return new_file_string
+    new_file_string = new_file_string.rstrip('\n') #Remove last '\n' in string
+    return new_file_string, target_variable
                 
 def InfixToPostifx(infix):
     """
@@ -123,7 +135,9 @@ def InfixToPostifx(infix):
 def main():
     read_file = open("datei.c")
     write_file = open("other_datei.c", "w")
-    renamed_expressions = variable_renaming(read_file)
+    renamed_expressions, target_variable = variable_renaming(read_file)
+    print("Zielvariablen: ")
+    print(target_variable)
     file_string = ""
     for line in renamed_expressions.split('\n'):
         file_string += InfixToPostifx(line)
